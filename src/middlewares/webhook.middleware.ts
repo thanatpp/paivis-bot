532845c2a4ba086679ca83aca64ce66fd219e0c8
thanatpp <spy.fo3@gmail.com>
2023-09-14 20:12:31 +0700

    feat: logger

---
+++ src/middlewares/webhook.middleware.ts
@@ -0,0 +1,31 @@
+const line = require("@line/bot-sdk");
+import { Context } from "baojs";
+import { HEADER_LINE_SIGNATURE, QUERY_API_KEY } from "../utils/constant";
+
+export async function webhookMiddleware(ctx: Context) {
+  const key = ctx.query.get(QUERY_API_KEY);
+  if (process.env.API_KEY !== key) {
+    return ctx.sendEmpty({ status: 401 }).forceSend();
+  }
+  ctx.query.delete(QUERY_API_KEY);
+
+  const req = await ctx.req.json();
+  const signature = ctx.headers.get(HEADER_LINE_SIGNATURE);
+  if (
+    signature === null ||
+    !isRequestFromLine(JSON.stringify(req), signature)
+  ) {
+    return ctx.sendEmpty({ status: 403 }).forceSend();
+  }
+
+  ctx.extra["req"] = req;
+  return ctx;
+}
+
+function isRequestFromLine(body: string, signature: string) {
+  return line.validateSignature(
+    body,
+    process.env.LINE_YOUR_CHANNEL_SECRET,
+    signature
+  );
+}
--- src/routes/webhook.route.ts
+++ src/routes/webhook.route.ts
@@ -1,31 +1,11 @@
 import Bao from "baojs";
-import { validateSignature } from "@line/bot-sdk";
 import webhookController from "../controllers/webhook.controller";
-import { WebhookRequestBody } from "@line/bot-sdk";
+import { webhookMiddleware } from "../middlewares/webhook.middleware";
+import { loggerInbound, loggerOutbound } from "../utils/logger";
 
 export default function webhookRoute(app: Bao) {
-  app.before(async (ctx) => {
-    const key = ctx.query.get("apiKey");
-    if (process.env.API_KEY !== key) {
-      return ctx.sendEmpty({ status: 401 }).forceSend();
-    }
-
-    const request: WebhookRequestBody = await ctx.req.json();
-    const signature = ctx.headers.get("X-Line-Signature");
-    if (
-      signature === null ||
-      !validateSignature(
-        JSON.stringify(request),
-        process.env.LINE_YOUR_CHANNEL_SECRET,
-        signature
-      )
-    ) {
-      return ctx.sendEmpty({ status: 403 }).forceSend();
-    }
-
-    ctx.extra["req"] = request;
-    return ctx;
-  });
-
+  app.before(webhookMiddleware);
+  app.before(loggerInbound);
   app.post("/webhook", webhookController);
+  app.after(loggerOutbound);
 }
