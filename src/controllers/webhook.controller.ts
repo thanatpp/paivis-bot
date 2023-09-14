73d5ab9027d5071418888d077b7dff93e493b376
thanatpp <spy.fo3@gmail.com>
2023-09-14 20:12:31 +0700

    feat: logger

--- src/controllers/webhook.controller.ts
+++ src/controllers/webhook.controller.ts
@@ -1,20 +1,9 @@
 import { Context } from "baojs";
-import { Client, WebhookRequestBody, WebhookEvent } from "@line/bot-sdk";
-import { config } from "../config/line.config";
+import { Client, WebhookRequestBody } from "@line/bot-sdk";
+import { config } from "../configs/line.config";
 
 export default function webhookController(ctx: Context) {
   const req = ctx.extra.req as WebhookRequestBody;
   const client = new Client(config());
-  replyMessage(client, req.events);
-  return ctx.sendText("success");
-}
-
-function replyMessage(client: Client, events: WebhookEvent[]) {
-  events.forEach(async (e: WebhookEvent) => {
-    if (e.type === "message" && e.message.type === "text")
-      await client.replyMessage(e.replyToken, {
-        type: "text",
-        text: e.message.text,
-      });
-  });
+  return ctx.sendEmpty({ status: 200 });
 }
