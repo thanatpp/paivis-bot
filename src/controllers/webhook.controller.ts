7f43ad11e1f4d8b31d73fe03ba962e9270129a06
thanatpp <spy.fo3@gmail.com>
2023-09-17 02:16:31 +0700

    fix: resilve conflict

--- src/controllers/webhook.controller.ts
+++ src/controllers/webhook.controller.ts
@@ -1,7 +1,6 @@
 import { Context } from "baojs";
 import { Client, WebhookRequestBody } from "@line/bot-sdk";
 import { config } from "../configs/line.config";
-<<<<<<< HEAD
 import webhookService from "../services/webhook.service";
 
 export default async function webhookController(ctx: Context) {
@@ -14,11 +13,4 @@ export default async function webhookController(ctx: Context) {
     console.log(err);
     return ctx.sendEmpty({ status: 500 });
   }
-=======
-
-export default function webhookController(ctx: Context) {
-  const req = ctx.extra.req as WebhookRequestBody;
-  const client = new Client(config());
-  return ctx.sendEmpty({ status: 200 });
->>>>>>> 6c4b02294a09bb1b634e9545e16455d586356f3f
 }
